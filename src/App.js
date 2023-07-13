0ece5922cbb93ca357a4822dd7466ccc00e4e1b5
xinjiyuan97 <xinjiyuan97@163.com>
2023-07-13 17:11:14 +0800

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from '@douyinfe/semi-ui';
+import dayjs from 'dayjs'
+
+
+import React from 'react';
+import { Nav, Avatar, Dropdown, Select, Button } from '@douyinfe/semi-ui';
+import { IconStar, IconUser, IconUserGroup, IconSetting, IconEdit, IconLanguage, IconSemiLogo } from '@douyinfe/semi-icons';
 
 function App() {
+  const { Header, Footer, Content } = Layout;
+
+  const items = [
+      { itemKey: 'user', text: '用户管理', icon: <IconUser /> },
+      { itemKey: 'union', text: '活动管理', icon: <IconStar /> },
+      {
+          itemKey: 'union-management',
+          text: '任务管理',
+          icon: <IconUserGroup />,
+          items: ['公告设置', '任务查询', '信息录入']
+      },
+      {
+          itemKey: 'approve-management',
+          text: '审批管理',
+          icon: <IconEdit />,
+          items: [
+              '入驻审核',
+              {
+                  itemKey: 'operation-management',
+                  text: '运营管理',
+                  items: [
+                      '人员管理',
+                      '人员变更'
+                  ]
+              }
+          ]
+      },
+      {
+          text: '任务平台',
+          icon: <IconSetting />,
+          itemKey: 'job',
+          items: ['任务管理', '用户任务查询'],
+      },
+  ];
+
+  const renderHorizontal = () => {
+    return (
+      <Nav
+        style={{ width: '100%' }}
+        mode={'horizontal'}
+        onSelect={key => console.log(key)}
+        header={{
+            logo: <IconSemiLogo style={{ height: '36px', fontSize: 36 }} />,
+            text: 'Semi 运营后台'
+        }}
+        footer={
+          <>
+            <Select defaultValue='Chinese' style={{ width: 120, marginRight: 10 }} insetLabel={<IconLanguage />}>
+              <Select.Option value='Chinese'>中文</Select.Option>
+              <Select.Option value='English'>English</Select.Option>
+              <Select.Option value='Korean'>한국어</Select.Option>
+              <Select.Option value='Japanese'>日本語</Select.Option>
+            </Select>
+            <Button style={{ marginRight: 10 }}>切换至全球版</Button>
+            <Dropdown
+              position="bottomRight"
+              render={
+                  <Dropdown.Menu>
+                      <Dropdown.Item>详情</Dropdown.Item>
+                      <Dropdown.Item>退出</Dropdown.Item>
+                  </Dropdown.Menu>
+              }
+            >
+                <Avatar size="small" color='light-blue' style={{ margin: 4 }}>BD</Avatar>
+                <span>Bytedancer</span>
+            </Dropdown>
+          </>
+        }
+      />
+    );
+  }
+
+  const renderVertical = () => {
+    return (
+        <Nav
+          style={{height: '100vh'}}
+          items={items}
+          onSelect={key => console.log(key)}
+          footer={{
+              collapseButton: true,
+          }}
+        />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{height: '100vh'}}>
+        {renderHorizontal()}
+        {renderVertical()}
+    </Layout>
   );
 }
 
